6d7f101da99972ef1b0d3861923917c00598fc95
Сидоров Юра <Odessa202@gmail.com>
2020-05-09 22:40:12 +0300

    added new methods

--- Snake/Display/ConsoleDisplay.h
+++ Snake/Display/ConsoleDisplay.h
@@ -22,9 +22,11 @@ public:
 	void ShowObject(T obj, int x = 0, int y = 0) override;
 	void ShowObjects(T* objs, int len, int startX = 0, int startY = 0) override;
 	void ShowObjects(T** objs, int row, int col, int startX = 0, int startY = 0) override;
-	void ShowText(wchar_t* text, int len, int startX = 0, int startY = 0) override;
+	void ShowText(wchar_t* text, int startX = 0, int startY = 0) override;
 	void ShowText(std::wstring text, int startX = 0, int startY = 0) override;
 	void ShowTime(Time& time, int x = 0, int y = 0) override;
+	void ShowNumber(int value, int x = 0, int y = 0) override;
+	void ShowNumber(float value, int x = 0, int y = 0) override;
 };
 
 template<typename T>
@@ -119,7 +121,7 @@ inline void ConsoleDisplay<T>::ShowObjects(T** objs, int row, int col, int start
 }
 
 template<typename T>
-inline void ConsoleDisplay<T>::ShowText(wchar_t* text, int len, int startX, int startY)
+inline void ConsoleDisplay<T>::ShowText(wchar_t* text, int startX, int startY)
 {
 	_mutex.lock();
 	_cursorCoord.X = startX;
@@ -151,5 +153,27 @@ inline void ConsoleDisplay<T>::ShowTime(Time& time, int x, int y)
 	if (time.Hour() > 0)
 		std::wcout << time.Hour() << L":";
 	std::wcout << time.Minute() << L":" << time.Second();
+	_mutex.unlock();
+}
+
+template<typename T>
+inline void ConsoleDisplay<T>::ShowNumber(int value, int x, int y)
+{
+	_mutex.lock();
+	_cursorCoord.X = x;
+	_cursorCoord.Y = y;
+	SetConsoleCursorPosition(_handle, _cursorCoord);
+	std::wcout << value;
+	_mutex.unlock();
+}
+
+template<typename T>
+inline void ConsoleDisplay<T>::ShowNumber(float value, int x, int y)
+{
+	_mutex.lock();
+	_cursorCoord.X = x;
+	_cursorCoord.Y = y;
+	SetConsoleCursorPosition(_handle, _cursorCoord);
+	std::wcout << value;
 	_mutex.unlock();
 }
--- Snake/Display/IDisplay.h
+++ Snake/Display/IDisplay.h
@@ -11,8 +11,10 @@ public:
 	virtual void ShowObject(T obj, int x = 0, int y = 0) = 0;
 	virtual void ShowObjects(T* objs, int len, int startX = 0, int startY = 0) = 0;
 	virtual void ShowObjects(T** objs, int row, int col, int startX = 0, int startY = 0) = 0;
-	virtual void ShowText(wchar_t* text, int len, int startX = 0, int startY = 0) = 0;
+	virtual void ShowText(wchar_t* text, int startX = 0, int startY = 0) = 0;
 	virtual void ShowText(std::wstring text, int startX = 0, int startY = 0) = 0;
 	virtual void ShowTime(Time& time, int x = 0, int y = 0) = 0;
+	virtual void ShowNumber(int value, int x = 0, int y = 0) = 0;
+	virtual void ShowNumber(float value, int x = 0, int y = 0) = 0;
 };
 
